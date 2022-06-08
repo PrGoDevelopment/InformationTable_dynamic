5369bab5166687a98fcfbffbb49509c64cc19a8c
PrGoDevelopment <prgo.development@gmail.com>
2022-06-08 17:41:25 -0300

    Update

--- app/src/main/java/com/example/informationtable/TableAdapter_Headers.java
+++ app/src/main/java/com/example/informationtable/TableAdapter_Headers.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +18,7 @@ public class TableAdapter_Headers extends RecyclerView.Adapter<TableAdapter_Head
 {
     Context ctx;
     List<String> header_list;
+
 
     public TableAdapter_Headers(Context ctx, List<String> header_list){
         this.ctx = ctx;
@@ -34,6 +37,33 @@ public class TableAdapter_Headers extends RecyclerView.Adapter<TableAdapter_Head
         if(header_list != null && header_list.size() > 0)
         {
             holder.txt_header.setText(header_list.get(position));
+
+
+
+
+//            final String[] str = {"one","two","three","asdfgf"};
+//            final RelativeLayout rl = (RelativeLayout) holder.itemView.findViewById(R.id.rl);
+//            final TextView[] tv = new TextView[10];
+//
+//            for (int i=0; i<str.length; i++)
+//            {
+//                tv[i] = new TextView(ctx);
+//                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams
+//                        ((int) RelativeLayout.LayoutParams.WRAP_CONTENT,(int) RelativeLayout.LayoutParams.WRAP_CONTENT);
+//                params.leftMargin = 50;
+//                params.topMargin  = i*50;
+//                tv[i].setText(str[i]);
+//                tv[i].setTextSize((float) 20);
+//                tv[i].setPadding(20, 50, 20, 50);
+//                tv[i].setLayoutParams(params);
+//                rl.addView(tv[i]);
+//            }
+
+
+
+
+
+
         }else{
             return;
         }
@@ -46,12 +76,39 @@ public class TableAdapter_Headers extends RecyclerView.Adapter<TableAdapter_Head
 
     public class ViewHolder extends RecyclerView.ViewHolder
     {
-        TextView txt_header;
+        TextView txt_header, txt_valueTable;
+        LinearLayout ll_mainLayoutTable;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             txt_header = itemView.findViewById(R.id.txt_header);
+            txt_valueTable = itemView.findViewById(R.id.txt_valueTable);
+            ll_mainLayoutTable = itemView.findViewById(R.id.ll_mainLayoutTable);
+
+            RelativeLayout rl_main = (RelativeLayout) itemView.findViewById(R.id.rl_main);
+
+
+
+            for(int i = 0; i < 3; i++){
+                //rl_main.addView(txt_valueTable);
+
+                TextView t = new TextView(ctx);
+
+                //ll_mainLayoutTable.addView(t);
+                rl_main.addView(t);
+            }
+
+
+
+
+
+
+
+
+
+
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
